02ab060a796d0c5de71fa422f63eebb1d691f63c
Syafiq Hamidun <syafiqhamidun10@gmail.com>
2024-07-04 16:18:41 +0800

    fetching data from backend using useEffect

--- src/component/ListCard.jsx
+++ src/component/ListCard.jsx
@@ -1,13 +1,29 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
 /* eslint-disable no-undef */
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import NewPost from "./NewPost"
 import TextCard from "./textCard"
 import Modal from "./Modal";
 
 function ListCard({isPosting, onStopPosting}) {
+    // fetch('http://localhost:8080/posts')
+    //     .then(response => response.json()) // The line takes this Response object and calls its .json() method, which parses the response body as JSON. This returns another Promise that resolves to the parsed JSON data.
+    //     .then(data => {
+    //         setPosts(data.posts) // updates the local state with the posts received from the server. data.posts refers to the array of posts within the JSON response.
+    //     })
 
     const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        async function fetchPosts() {
+            const response = await fetch('http://localhost:8080/posts');
+            const resData = await response.json();
+            setPosts(resData.posts)
+        }
+
+        fetchPosts();
+    }, [])
 
     function addPostHandler(postData){ //This line defines a function named addPostHandler which takes one parameter, postData. This parameter represents the data for the new post that needs to be added.
         fetch('http://localhost:8080/posts' , {   //fetch data from local dummy server 
