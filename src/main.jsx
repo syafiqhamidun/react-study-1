63acc414f127ba3422243ac59245d1663190f142
Syafiq Hamidun <syafiqhamidun10@gmail.com>
2024-07-04 17:20:23 +0800

    Adding route & layout for multiple pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
+/* eslint-disable no-unused-vars */
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+
 import App from './App.jsx'
 import './index.css'
+import NewPost from './component/NewPost.jsx'
+import RootLayout from './routes/RootLayout.jsx'
+
+const router = createBrowserRouter([
+  { path: '/' , element: <RootLayout/>, children: [
+    { path: '/' , element: <App/> },
+    { path: '/create-post' , element: <NewPost/>}, 
+  ]},
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
